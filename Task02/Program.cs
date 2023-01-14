cb0dd770b51be2b0ce7093751bd35f9e2f28c9df
Einzel <einzel0102@mail.ru>
2023-01-14 16:19:46 +0800

    задача 26

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 26: Напишите программу, которая принимает
+//на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+int SumDigit(int digit)
+{
+    int count = 1;
+    while (digit >= 10)
+    {
+      digit = digit / 10;
+      count = count + 1; 
+    }
+    return count;
+}
+
+Console.WriteLine("Введите целое натурально число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = SumDigit(number);
+Console.WriteLine(sumDigit);
+
+
+
